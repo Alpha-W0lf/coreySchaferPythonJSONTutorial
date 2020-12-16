b4eb6b899893fb4d82a2199c5d4476556e0ef42f
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2020-12-15 19:05:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # notes from following youtube tutorial: https://www.youtube.com/watch?v=9N6a-VLBa2I
 
+# JavaScript Object Notation
+
+import json
+
+people_string = '''
+{
+    "people": [
+    {
+        "name": "John Smith",
+        "phone": "615-555-7164",
+        "emails": ["johnsmith@bogusemail.com", "john.smith@work-place.com"],
+        "has_license": false
+    },
+    {
+        "name": "Jane Doe",
+        "phone": "560-555-5153",
+        "emails": null,
+        "has_license": true
+    }
+    ]
+}
+'''
+
+data = json.loads(people_string)
+print(type(data))
+print(data)
